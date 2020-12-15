89b99127aa945d9517043f792ee192e2edbdb8e2
Nam-Ki-Bok <nkb7714@naver.com>
2020-12-15 21:45:32 +0900

    [2020.12.15]

--- src/Rank.java
+++ src/Rank.java
@@ -1,12 +1,19 @@
-import java.awt.*;
-import javax.swing.*;
-import java.io.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Rank extends JPanel {
 	
@@ -23,15 +30,12 @@ public class Rank extends JPanel {
 	public void connectDB() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-//			System.out.println("드라이버 로드 성공");
 		} catch (ClassNotFoundException e1) {
 			e1.printStackTrace();
 		}
 
 		try { // 데이터베이스를 연결하는 과정
-//			System.out.println("데이터베이스 연결 준비...");
 			con = DriverManager.getConnection(url, userid, pwd);
-//			System.out.println("데이터베이스 연결 성공");
 		} catch (SQLException e2) {
 			e2.printStackTrace();
 		}
@@ -50,7 +54,6 @@ public class Rank extends JPanel {
 	
 	public Rank() {
 		connectDB();
-		
 		Color backcolor = new Color(246, 223, 170);
 		setPreferredSize(new Dimension(600, 700));
 		setBackground(backcolor);
